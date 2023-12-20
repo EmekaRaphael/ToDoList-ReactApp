40ce367a9a76b95d5e25d03d8ff83eaa30b3922c
EmekaRaphael <emekaraphael13@gmail.com>
2023-12-21 00:49:07 +0100

    updae

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return <div />;
+
+
+  const [count, setCount] = useState(0);
+
+
+  function increase() {
+    setCount(count - 1);
+  }
+
+  function reduce() {
+    setCount(count + 1);
+  }
+
+
+  return (<div className="container">
+    <h1>{count}</h1>
+    <button onClick={increase}>-</button>
+    <button onClick={reduce}>+</button>
+  </div>)
 }
 
 export default App;
