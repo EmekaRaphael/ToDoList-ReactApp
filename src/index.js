8bd88f7a53f05a71dbc8c158fabb7eb2ff687203
EmekaRaphael <emekaraphael13@gmail.com>
2023-12-24 17:56:42 +0100

    ugj

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
 //CHALLENGE: Make this app work by applying what you've learnt.
 //1. When new text is written into the input, its state should be saved.
 //2. When the add button is pressed, the current data in the input should be
